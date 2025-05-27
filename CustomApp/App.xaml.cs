3800f28b5b4bc554027b8860fba29ef25aa48db4
Thierry Morre <thierry.morre@cognitic.be>
2025-05-27 21:23:21 +0200

    Ajoutez des fichiers projet.

---
+++ CustomApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using Tools.Mvvm;
+
+namespace CustomApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : OptecApp
+    {
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        public App()
+        {
+            Console.WriteLine(Configuration["Test"]);
+            Console.WriteLine(ServiceProvider.GetService<IConfiguration>());
+        }
+    }
+
+}
---
+++ Tools.Mvvm/OptecApp.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+
+namespace Tools.Mvvm
+{
+    public abstract class OptecApp : Application
+    {
+        public IServiceProvider ServiceProvider { get; }
+        public IConfiguration Configuration { get; }
+
+        protected OptecApp()
+        {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            services.AddSingleton(Configuration);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        protected abstract void ConfigureServices(IServiceCollection services);
+    }
+}
